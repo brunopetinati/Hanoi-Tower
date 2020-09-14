6c49d1d1cce9ca6721877cae3ea74942489653a1
Bruno Petinati <brunopetinati@hotmail.com>
2020-09-14 17:24:24 -0300

    comentários adicionados

---
+++ script.js
@@ -0,0 +1,18 @@
+//click para selecionar origem
+//click para selecionar destino
+//variável para registar o modo que o jogador está. Seria armazenar a posição?
+
+//adicionar handler em cada uma das três torres.
+//usar event.currentTarget dentro do handler de evento para determinar qual torre foi clicada.
+
+//Usar .childElementCount para saber quantos discos estão em uma torre.
+//Usar a propriedade .lastElementChild para saber qual é o disco no topo da torre.
+
+//Usar  .appendChild() para adicionar um disco a uma torre 
+//Obs: quando você usa appendChild em um elemento que já tem um pai, ele é automaticamente removido do pai anterior e adicionado no novo.
+
+//Usar a propriedade Element.clientWidth para pegar o tamanho dos discos.
+
+// --- --- --- Passos
+// 1. Preciso saber o que é um handler de evento. 
+
