651c958d324390f6228b6c9b5337e6ff46403430
Bruno Petinati <brunopetinati@hotmail.com>
2020-09-14 23:37:59 -0300

    última tentativa antes de pedir auxílio aos coaches

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 //click para selecionar destino
 //variável para registar o modo que o jogador está. Seria armazenar a posição?
 
-//adicionar handler em cada uma das três torres.
+//adicionar handler em cada uma das três torres. OK
 //usar event.currentTarget dentro do handler de evento para determinar qual torre foi clicada.
 
 //Usar .childElementCount para saber quantos discos estão em uma torre.
@@ -14,30 +14,36 @@
 //Usar a propriedade Element.clientWidth para pegar o tamanho dos discos.
 
 // --- --- --- Passos
-// 1. Preciso saber o que é um handler de evento. 
-// 2. como remover uma barra e adicionar à outra div
-// 3. como mover o disco para a div que eu quero
+// 1. Preciso saber o que é um handler de evento. OK
+// 2. como remover uma barra e adicionar à outra div. OK
+// 3. como mover o disco para a div que eu quero. OK
 // 4. criei várias div no html. Será necessário colocar todas na memória com let? Ou será melhor usar createElement?
+// 5. como clico primeiro para reservar a div, e depois clico no próximo para appendChild nela?
 
 /* Discos */
 
-let disco1 = document.getElementById("disco1")
+/* let disco1 = document.getElementById("disco1")
 let disco2 = document.getElementById("disco2")
 let disco3 = document.getElementById("disco3")
-let disco4 = document.getElementById("disco4")
+let disco4 = document.getElementById("disco4") */
 
 /* Torres */
 
-let inicio = document.getElementById("inicio")
-let meio = document.getElementById("meio")
-let final = document.getElementById("final")
+let torre1 = document.getElementById("inicio")
+let torre2 = document.getElementById("meio")
+let torre3 = document.getElementById("final")
 
-function move() {
-    meio.appendChild(disco1)
+let disco = [] 
+
+torre1.onclick = function() {
+    
 }
 
+torre2.onclick = function() {
+    torre2.appendChild(disco1) 
+}
 
-disco1.onclick = move
-disco2.onclick = move
-disco3.onclick = move
-disco4.onclick = move
+torre3.onclick = function() {
+
+}
+
